70ce9a27f2a1d178b743f29143505918f3975d0a
Meysam Kia <msmkia@outlook.com>
2021-01-12 00:49:09 +0330

    finished qa5

--- projects/3. Adventure Game/adv-world.scm
+++ projects/3. Adventure Game/adv-world.scm
@@ -1,3 +1,6 @@
+(load "tables.scm")
+(load "obj.scm")
+(load "adv.scm")
 ;;;  Data for adventure game.  This file is adv-world.scm
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -48,8 +51,36 @@
 (define hacker (instantiate person 'hacker 61A-Lab))
 (define nasty (instantiate thief 'nasty Sproul-Plaza))
 
-(define (sproul-hall-exit)
-   (error "You can check out any time you'd like, but you can never leave"))
+;; Question A3
+;; whenever a person goes to a new place, the place gets an 'ENTER message.
+;; the place the person previously inhabited gets an 'EXIT message.
+;; When the place gets the message, it calls each procedure on its list of
+;; ENTRY-PROCEDURES or EXIT-PROCEDURES as appropriate.
+;; Places have the following methods defined for manipulating these lists of procedures:
+;; ADD-ENTRY-PROCEDURE, ADD-EXIT-PROCEDURE, REMOVE-ENTRY-PROCEDURE, REMOVE-EXIT-PROCEDURE,
+;; and CLEAR-ALL-PROCS. You can read their definitions in the code.
+
+;; Sproul Hall has a particularly obnoxious exit procedure attached to it.
+;; Fix sproul-hall-exit so that it counts how many times it gets called,
+;; and stops being obnoxious after the third time.
+
+;; Remember that the exit-procs list contains procedures, not names of procedures!
+;; It's not good enough to redefine sproul-hall-exit,
+;; since Sproul Hall's list of exit procedures still contains the old procedure.
+;; The best thing to do is just to load adv-world.scm again,
+;; which will define a new Sproul Hall and add the new exit procedure.
+
+;; (define (sproul-hall-exit)
+;;    (error "You can check out any time you'd like, but you can never leave"))
+
+(define sproul-hall-exit
+	(let ((x 0))
+		(lambda ()
+			(set! x (+ x 1))
+			(if (> x 3)
+					(print "You can leave now")
+					(error "You can check out any time you'd like, but you can never leave")))))
+
 
 (define (bh-office-exit)
   (print "What's your favorite programming language?")
@@ -80,3 +111,216 @@
 
 (define coffee (instantiate thing 'coffee))
 (ask Intermezzo 'appear coffee)
+
+
+;; Q1
+;; Instantiate a new Person object to represent yourself.
+;; Put yourself in a new place called dormitory (or wherever you live)
+;; and connect it to campus so that it is a reachable place.
+;; Create a place called kirin, north of soda. Put a thing called potstickers there.
+;; Then give the necessary commands to move your character to kirin,
+;; take the potstickers, then move yourself to where Brian is,
+;; put down the potstickers, and have Brian take them.
+;; Then go back to the lab and get back to work.
+
+;; Q1 answer
+;; define dormitory
+(define dormitory (instantiate place 'dormitory))
+
+;; connect dormitory to pimentel
+(can-go dormitory 'east Pimentel)
+(can-go Pimentel 'west dormitory)
+
+;; define mey in dormitory
+(define mey (instantiate person 'Mey dormitory))
+
+;; define kirin
+(define kirin (instantiate place 'Kirin))
+
+;; connect kirin to soda
+(can-go kirin 'south Soda)
+(can-go Soda 'north kirin)
+
+;; define potstickers and put it in kirin
+(define potstickers (instantiate thing 'potstickers))
+(ask kirin 'appear potstickers)
+
+;; command mey to go to kirin from dormitory
+(ask mey 'go 'east)
+(ask mey 'go 'north)
+(ask mey 'go 'north)
+
+;; mey takes potstickers
+(ask mey 'take potstickers)
+
+;; mey goes to bh
+(ask mey 'go 'south)
+(ask mey 'go 'up)
+(ask mey 'go 'west)
+
+;; mey put potstickers
+(ask mey 'lose potstickers)
+
+;; when mey goes to some place, place will have his things
+;; (ask BH-Office 'appear potstickers)
+
+;; brian takes the potstickers
+(ask Brian 'take potstickers)
+
+;; mey goes to lab
+(ask mey 'go 'east)
+(ask mey 'go 'down)
+(ask mey 'go 'south)
+(ask mey 'go 'south)
+
+;; -----------------------------------------------------------------------------
+
+;; Q2a What kind of thing is the value of variable Brian?
+;; Hint: What is returned by STk in the following situation:
+;; > Brian
+;; answer: procedure
+
+;; Q2b List all the messages that a Place understands.
+;; (You might want to maintain such a list for your own use,
+;; for every type of object, to help in the debugging effort.)
+
+;; answer:
+;; class: place:
+;; instantiation vars: name
+;; instance vars: directions-and-neighbors people entry-procs exit-procs
+;; methods: type neigbors exits look-in appear enter gone exit new-neigbor
+;;          add-entry-procedure add-exit-procedure remove-entry-procedure remove-exit-procedure
+;;          clear-all-procs
+
+;; class: person
+;; instantiation vars: name place
+;; instance vars: possessions saying
+;; methods: (type) (look-around) (take thing) (lose thing) (talk) (set-talk string)
+;;          (exits) (notice person) (go direction)
+;; init:    (ask place 'enter self)
+
+
+;; Q2c We have been defining a variable to hold each object in our world.
+;; For example, we defined bagel by saying:
+
+;; > (define bagel (instantiate thing 'bagel))
+;; This is just for convenience. Every object does not have to have a top-level definition.
+;; Every object DOES have to be constructed and connected to the world.
+;; For instance, suppose we did this:
+
+;; > (can-go Telegraph-Ave 'east (instantiate place 'Peoples-Park))
+
+;; ;;; assume Brian is at Telegraph
+;; > (ask Brian 'go 'east)
+;; What is returned by the following expressions and why?
+
+;; > (ask Brian 'place)
+;; answer: procedure. place is an instance var for person returning a place object
+
+;; > (let ((where (ask Brian 'place)))
+;;        (ask where 'name))
+;; answer: Peoples-Park: the place is found and its name is asked
+
+;; > (ask Peoples-park 'appear bagel)
+;; answer: error. because People-park is an object and not bound to any reference
+
+
+;; Q2d The implication of all this is that there can be multiple names for objects.
+;; One name is the value of the object's internal name variable.
+;; In addition, we can define a variable at the top-level to refer to an object.
+;; Moreover, one object can have a private name for another object.
+;; for example, Brian has a variable place which is currently bound to the object
+;; that represents People's Park. Some examples to think about:
+
+;; > (eq? (ask Telegraph-Ave 'look-in 'east) (ask Brian 'place))
+;; #t
+;; > (eq? (ask Brian 'place) 'Peoples-Park)
+;; #f
+;; > (eq? (ask (ask Brian 'place) 'name) 'Peoples-Park)
+;; #t
+
+;; Suppose we type the following into STk:
+
+;; > (define computer (instantiate thing 'Durer))
+;; Which of the following is correct? Why?
+
+;; (ask 61A-Lab 'appear computer)
+;; or
+
+;; (ask 61A-Lab 'appear Durer)
+;; or
+
+;; (ask 61A-Lab 'appear 'Durer)
+
+;; What is returned by (computer 'name)? Why?
+
+;; Answer:
+;; we want the object name to appear in the place not its internal name.
+;; so the first one is correct.
+
+;; by (computer 'name) a procedure is returned.
+;; calling an object on a message returns the dispatch
+;; calling ask on the dispatch runs the method associated by that message
+
+
+;; codes for Qa4 part 1
+
+(define singer (instantiate person 'rick Sproul-Plaza))
+(ask singer 'set-talk "My funny valentine, sweet comic valentine")
+(define preacher (instantiate person 'preacher Sproul-Plaza))
+(ask preacher 'set-talk "Praise the Lord")
+(define street-person (instantiate person 'harry Telegraph-Ave))
+(ask street-person 'set-talk "Brother, can you spare a buck")
+
+;; codes for Qa4 part 2
+
+(define warehouse (instantiate locked-place 'warehouse))
+(can-go Intermezzo 'south warehouse)
+(can-go warehouse 'north Intermezzo)
+
+;; code for qa5
+;; garages
+(define g1 (instantiate garage 'g1))
+(define g2 (instantiate garage 'g2))
+(can-go g1 'north 61A-Lab)
+(can-go 61A-Lab 'south g1)
+(can-go g2 'west Pimentel)
+(can-go Pimentel 'east g2)
+
+;; vehicles
+(define bmw (instantiate thing 'bmw))
+(define glx (instantiate thing 'glx))
+(define toyota (instantiate thing 'toyota))
+(define honda (instantiate thing 'honda))
+
+;; vehicles in places
+(ask Pimentel 'appear bmw)
+(ask Pimentel 'appear glx)
+(ask Pimentel 'appear toyota)
+(ask g1 'appear honda)									; a vehicle without owner
+
+;; people
+(define jj (instantiate person 'jj Pimentel))
+(ask mey 'go 'north)										; mey at pimentel
+
+(ask mey 'take toyota)
+(ask jj 'take bmw)
+
+;; checks if park happens if vehicle not there
+;; (ask g2 'park bmw)
+
+(ask jj 'go 'east)
+
+(ask g2 'park bmw)
+(ask jj 'go 'west)
+;; checks if unpark happens if the ticket owner is not there
+;; (ask g2 'unpark (car (ask jj 'possessions)))
+
+(ask jj 'go 'east)
+(ask g2 'unpark (car (ask jj 'possessions)))
+
+;; checks parking a vehicle without owner
+;; (ask g2 'park honda)
+
+
+;; there are other things to check by maybe later
--- projects/3. Adventure Game/adv.scm
+++ projects/3. Adventure Game/adv.scm
@@ -25,9 +25,16 @@
 	(method (enter new-person)
 					(if (memq new-person people)
 							(error "Person already in this place" (list name new-person)))
+
+					;; ------------------------------------------------------------------
+					;; answer for QA4 part 1
+					(for-each (lambda (p) (ask p 'notice new-person)) people)
+					;; ------------------------------------------------------------------
+
 					(set! people (cons new-person people))
 					(for-each (lambda (proc) (proc)) entry-procs)
 					'appeared)
+
 	(method (gone thing)
 					(if (not (memq thing things))
 							(error "Disappearing thing not here" (list name thing)))
@@ -56,7 +63,20 @@
 	(method (clear-all-procs)
 					(set! exit-procs '())
 					(set! entry-procs '())
-					'cleared) )
+					'cleared)
+;;  ------------------------------------------------------------------
+	;; answer QA4 part 2
+	(method (may-enter? person) #t))
+
+(define-class (locked-place name)
+	(parent (place name))
+	(instance-vars
+	 (locked? #t))
+	(method (may-enter? person)
+					(not locked?))
+	(method (unlock)
+					(set! locked? #f)))
+;;  ------------------------------------------------------------------
 
 
 (define-class (person name place)
@@ -103,6 +123,8 @@
 					(let ((new-place (ask place 'look-in direction)))
 						(cond ((null? new-place)
 									 (error "Can't go" direction))
+									((not (ask new-place 'may-enter? self))
+									 (error "The place is locked for you"))
 									(else
 									 (ask place 'exit self)
 									 (announce-move name place new-place)
@@ -114,32 +136,43 @@
 									 (set! place new-place)
 									 (ask new-place 'enter self))))) )
 
-(define thing
-  (let ()
-    (lambda (class-message)
-      (cond
-       ((eq? class-message 'instantiate)
-	(lambda (name)
-	  (let ((self '()) (possessor 'no-one))
-	    (define (dispatch message)
-	      (cond
-	       ((eq? message 'initialize)
-		(lambda (value-for-self)
-		  (set! self value-for-self)))
-	       ((eq? message 'send-usual-to-parent)
-		(error "Can't use USUAL without a parent." 'thing))
-	       ((eq? message 'name) (lambda () name))
-	       ((eq? message 'possessor) (lambda () possessor))
-	       ((eq? message 'type) (lambda () 'thing))
-	       ((eq? message 'change-possessor)
-		(lambda (new-possessor)
-		  (set! possessor new-possessor)))
-	       (else (no-method 'thing))))
-	    dispatch)))
-       (else (error "Bad message to class" class-message))))))
+;; Q2e We have provided a definition of the Thing class
+;; that does not use the object-oriented programming syntax described in the handout.
+;; Translate it into the new notation.
 
+;; (define thing
+;; 	(let ()
+;; 		(lambda (class-message)
+;; 			(cond
+;; 			 ((eq? class-message 'instantiate)
+;; 				(lambda (name)
+;; 					(let ((self '()) (possessor 'no-one))
+;; 						(define (dispatch message)
+;; 							(cond
+;; 							 ((eq? message 'initialize)
+;; 								(lambda (value-for-self)
+;; 									(set! self value-for-self)))
+;; 							 ((eq? message 'send-usual-to-parent)
+;; 								(error "Can't use USUAL without a parent." 'thing))
+;; 							 ((eq? message 'name) (lambda () name))
+;; 							 ((eq? message 'possessor) (lambda () possessor))
+;; 							 ((eq? message 'type) (lambda () 'thing))
+;; 							 ((eq? message 'change-possessor)
+;; 								(lambda (new-possessor)
+;; 									(set! possessor new-possessor)))
+;; 							 (else (no-method 'thing))))
+;; 						dispatch)))
+;; 			 (else (error "Bad message to class" class-message))))))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;  ------------------------------------------------------------------
+;; answer to Q2e
+(define-class (thing name)
+	(instance-vars
+	 (possessor 'no-one)
+	 (type 'thing))
+	(method (change-possessor person) (set! possessor person)))
+;;  ------------------------------------------------------------------
+
 ;;; Implementation of thieves for part two
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (define *foods* '(pizza potstickers coffee))
@@ -231,3 +264,148 @@
   (and (procedure? obj)
        (eq? (ask obj 'type) 'thing)))
 
+;; q2-f) Sometimes it's inconvenient to debug an object interactively
+;; because its methods return objects and we want to see the names of the objects.
+;; You can create auxiliary procedures for interactive use
+;; (as opposed to use inside object methods) that provide the desired information
+;; in printable form. For example:
+
+;; (define (name obj) (ask obj 'name))
+;; (define (inventory obj)
+;;     (if (person? obj)
+;;         (map name (ask obj 'possessions))
+;;         (map name (ask obj 'things))))
+
+;; write a procedure whereis that takes a person as its argument
+;; and returns the name of the place where that person is.
+
+(define (whereis person)
+	(let
+			((place (ask person 'place)))
+		(ask place 'name)))
+
+;; write a procedure owner that takes a thing as its argument and returns
+;; the name of the person who owns it.
+;; (Make sure it works for things that aren't owned by anyone.)
+
+(define (owner thing)
+	(let
+			((possessor (ask thing 'possessor)))
+		(if (eq? possessor 'no-one)
+				possessor
+				(ask possessor 'name))))
+
+
+;; Q A4 Part 1
+;; We've provided people with the ability to say something
+;; using the messages 'talk and 'set-talk.
+
+;; the talker is already at the place
+;; listener entering the place
+;; the listener sends an enter message to the place when entering.
+;; the talker has a notice method that when called, he talks.
+
+;; modify the enter method for places, so that in addition
+;; to what that method already does, it sends a notice message
+;; to each person in that place other than the person who is entering.
+;; The notice message should have the newly-entered person as an argument.
+;; (You won't do anything with that argument now, but you'll need it later.)
+
+;; Add the following to adv-world.scm:
+
+;; (define singer (instantiate person 'rick sproul-plaza))
+
+;; (ask singer 'set-talk "My funny valentine, sweet comic valentine")
+
+;; (define preacher (instantiate person 'preacher sproul-plaza))
+
+;; (ask preacher 'set-talk "Praise the Lord")
+
+;; (define street-person (instantiate person 'harry telegraph-ave))
+
+;; (ask street-person 'set-talk "Brother, can you spare a buck")
+
+;; QA4 part 2
+;; Invent a may-enter? message for places that takes a person as an argument and always returns #t.
+
+
+;; QA5
+;; Invent a special kind of place called a garage.
+;; Garages have two methods (besides the ones all places have): park and unpark.
+;; The park method takes a vehicle (a thing) as its argument.
+
+;; You'll also need a special kind of thing called a ticket.
+;; what's special about it is that it has a number as an instantiation variable.
+
+;; The person who possesses the vehicle will enter the garage,
+;; then ask to park it, so the vehicle should have entered the garage
+;; along with the person before the park message is sent.
+
+
+;; First check to be sure that the vehicle is actually in the garage.
+;; Then generate a ticket with a unique serial number.
+;; The counter for serial numbers should be shared among all garages,
+;; so that we don't get in trouble later trying to unpark a vehicle
+;; from one garage that was parked in a different garage.)
+;; Every ticket should have the name ticket.
+
+;; The file tables.scm contains an implementation of the table Abstract Data Type:
+;; constructor: (make-table) returns a new, empty table.
+;; mutator: (insert! key value table) adds a new key-value pair to a table.
+;; selector: (lookup key table) returns the corresponding value, or #f if
+;;                      the key is not in the table.
+
+
+;; Make a table entry with the ticket number as the key, and the vehicle as the value.
+;; Then ask the vehicle's owner to lose the vehicle and take the ticket.
+
+;; The unpark method takes a ticket as argument. First make sure the object
+;; you got is actually a ticket (by checking the name). Then look up the ticket number
+;; in the garage's table. If you find a vehicle, ask the ticket's owner to
+;; lose the ticket and take the vehicle. Also, insert #f in the table for that ticket number,
+;; so that people can't unpark the vehicle twice.
+
+
+;; Notes:
+;; A ticket only has one instantiation variable, a serial number. (e.g., (instantiate ticket 120)).
+;; A ticket is a thing with the name 'ticket
+;; A garage takes one instantiation variable, its name. (e.g., (instantiate garage 'soda-garage)).
+
+;; Do NOT define a new class for vehicles. You can assume that
+;; park is called with the correct argument.
+
+;; Parking a vehicle that is not owned by anyone should return an error.
+;; Unparking a vehicle that is not parked should return an error.
+
+(define-class (ticket num)
+	(parent (thing 'ticket)))
+
+(define-class (garage name)
+	(parent (place name))
+	(class-vars (counter 0))
+	(instance-vars (table (make-table)))
+	(method (park vehicle)
+					(cond ((not (memq vehicle (ask self 'things)))
+								 (error "the vehicle" (ask vehicle 'name) "is not in the garage" name))
+								((eq? (owner vehicle) 'no-one)
+								 (error "the vehicle" (ask vehicle 'name) "is not owned by anyone"))
+								(else
+								 (let ((tick (instantiate ticket (+ counter 1))))
+									 (begin
+										 (set! counter (+ counter 1))
+										 (insert! (ask tick 'num) vehicle table)
+										 (ask self 'appear tick)
+										 (ask (ask vehicle 'possessor) 'take tick)
+										 (ask (ask vehicle 'possessor) 'lose vehicle)
+										 'parked)))))
+	(method (unpark tick)
+					(if (not (eq? (ask tick 'name) 'ticket))
+							(error "not a ticket")
+							(let ((vehicle (lookup (ask tick 'num) table)))
+								(if vehicle
+										(begin
+											(ask (ask tick 'possessor) 'take vehicle)
+											(ask (ask tick 'possessor) 'lose tick)
+											(insert! (ask tick 'num) #f table)
+											'unparked)
+										(error "vehicle not parked in the garage"))))))
